5ec521cd6d1a3eaeba936ef43609e16a292d0753
Shawn <codingshawn@gmail.com>
2020-06-23 16:41:00 +0800

    68.5% correct guessess in 100000 tries after adding algorithmn for 3 way swap with 3 white pins

--- codebreaker.rb
+++ codebreaker.rb
@@ -46,8 +46,10 @@ class CodeBreaker
 
 	def computer_determine_order(black, guess)
 		case black
-		when 1..2
+		when 2
 			swap_pins(guess)
+		when 1
+			swap_pins_3_white(guess)
 		when 0
 			swap_pins_4_white(guess)
 		else #won't have 1 black pin only
@@ -69,7 +71,6 @@ class CodeBreaker
 	end
 
 	def swap_pins_4_white(guess)
-		#binding.pry	
 		to_swap = [1,2,3] #number of positions for pin 1 to swap to
 		swap1 = to_swap[rand(2)] # select a position for pin 1 to swap to
 		to_swap -= [swap1] #remaining positions
@@ -81,7 +82,20 @@ class CodeBreaker
 		temp1 = guess[to_swap[0]]
 		guess[to_swap[0]] = guess[to_swap[1]]
 		guess[to_swap[1]] = temp1
-		#binding.pry	
+		return guess
+	end
+
+	def swap_pins_3_white(guess)
+		to_swap = [0,1,2,3] #possible positions of pins
+		pin1 = to_swap[rand(3)]
+		to_swap -= [pin1] #remaining pins position
+		pin2 = to_swap[rand(2)]
+		to_swap -= [pin2]
+		pin3 = to_swap[rand(1)]
+		temp = guess[pin1] #3 way swap, not randomized again as initially already random
+		guess[pin1] = guess[pin2]
+		guess[pin2] = guess[pin3]
+		guess[pin3] = temp
 		return guess
 	end
 end
